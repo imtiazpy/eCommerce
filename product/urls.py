4d9be3c7a1625993b817fd8138e8edce19c20681
imtiazpy <ahmed.imti1994@gmail.com>
2022-04-09 19:19:07 +0600

    added delete item functionality to cart and checkout page

--- product/urls.py
+++ product/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cart/', cart, name="cart"),
     path('checkout/', checkout, name='checkout'),
     path('update_item/', updateItem, name='update_item'),
+    path('delete_item/', deleteItem, name='delete_item'),
 ]
